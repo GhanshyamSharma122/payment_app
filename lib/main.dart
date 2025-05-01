eab56f443d1de222595cc03d766c169c538f5654
shyam poudel <poudelshyam844@gmail.com>
2025-05-01 09:34:18 +0530

    dfdf

--- lib/main.dart
+++
@@ -1,7 +0,0 @@
-// lib/main.dart
-import 'package:flutter/material.dart';
-import 'package:payment_app/app.dart'; // Import the app.dart file we'll create next
-
-void main() {
-  runApp(const MyApp()); // Run the main application widget
-}
